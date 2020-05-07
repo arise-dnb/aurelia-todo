afe83df42f1513f97a4ec8892d6f0358310b8a2a
Hannes Jörk <hannes.joerk@t-systems.com>
2020-05-07 17:03:13 +0200

    Added router-view showing new template in main page

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import {Todo} from './todo';
+import {Router, RouterConfiguration} from 'aurelia-router';
+import {PLATFORM} from 'aurelia-framework';
 
 export class App {
   public message: string;
   public todoList: Todo[];
   public newItem: string;
+
+  router: Router;
 
   constructor() {
     this.message = 'Hello World!';
@@ -12,6 +16,13 @@ export class App {
     this.todoList.push(new Todo('Learn Aurelia'));
     this.todoList.push(new Todo('Finish work'));
     this.newItem = '';
+  }
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'Router Test';
+    config.map([
+      {route: '', name: 'home', moduleId: PLATFORM.moduleName('index'), title: 'Home'}
+    ]);
   }
 
   addTodo() {
@@ -23,11 +34,28 @@ export class App {
     this.todoList.splice(this.todoList.indexOf(todo), 1);
   }
 
-  configureRouter(config, router) {
-    config.title = 'Router Test';
+
+}
+
+/**
+import {Router, RouterConfiguration} from 'aurelia-router';
+import {inject, PLATFORM} from 'aurelia-framework';
+import {WebAPI} from './web-api';
+
+@inject(WebAPI)
+export class App {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.title = 'Contacts';
+    config.options.pushState = true;
+    config.options.root = '/';
     config.map([
-      {},
-      {}
-    ])
+      { route: '',              moduleId: PLATFORM.moduleName('no-selection'),   title: 'Select' },
+      { route: 'contacts/:id',  moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
+    ]);
+
+    this.router = router;
   }
 }
+ **/
